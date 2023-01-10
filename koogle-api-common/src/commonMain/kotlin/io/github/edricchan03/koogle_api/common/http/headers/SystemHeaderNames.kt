169d07efd448917e01479415615bd168ffeb8cb9
Edric <edric.chan.1997@gmail.com>
2023-01-10 17:12:49 +0800

    chore: add system header names as a separate file

---
+++ koogle-api-common/src/commonMain/kotlin/io/github/edricchan03/koogle_api/common/http/headers/SystemHeaderNames.kt
@@ -0,0 +1,67 @@
+package io.github.edricchan03.koogle_api.common.http.headers
+
+/**
+ * Corresponds to the `Authorization` HTTP header.
+ * @see SystemHeadersBuilder.authorization
+ */
+public const val authHeader: String = "Authorization"
+
+/**
+ * Corresponds to the `Content-Type` HTTP header.
+ * @see SystemHeadersBuilder.contentType
+ */
+public const val contentTypeHeader: String = "Content-Type"
+
+/**
+ * Corresponds to the `X-Goog-FieldMask` HTTP header.
+ * @see SystemHeadersBuilder.fields
+ */
+public const val fieldsHeader: String = "X-Goog-FieldMask"
+
+/**
+ * Corresponds to the `X-HTTP-Method-Override` HTTP header.
+ * @see SystemHeadersBuilder.httpMethodOverride
+ */
+public const val httpMethodOverrideHeader: String = "X-HTTP-Method-Override"
+
+/**
+ * Corresponds to the `X-Goog-Api-Key` HTTP header.
+ * @see SystemHeadersBuilder.apiKey
+ */
+public const val apiKeyHeader: String = "X-Goog-Api-Key"
+
+/**
+ * Corresponds to the `X-Goog-Quota-User` HTTP header.
+ * @see SystemHeadersBuilder.quotaUser
+ */
+public const val quotaUserHeader: String = "X-Goog-Quota-User"
+
+/**
+ * Corresponds to the `X-Goog-Api-Client` HTTP header.
+ * @see SystemHeadersBuilder.apiClient
+ */
+public const val apiClientHeader: String = "X-Goog-Api-Client"
+
+/**
+ * Corresponds to the `X-Goog-Request-Reason` HTTP header.
+ * @see SystemHeadersBuilder.requestReason
+ */
+public const val requestReasonHeader: String = "X-Goog-Request-Reason"
+
+/**
+ * Corresponds to the `X-Goog-User-Project` HTTP header.
+ * @see SystemHeadersBuilder.userProject
+ */
+public const val userProjectHeader: String = "X-Goog-User-Project"
+
+/**
+ * Corresponds to the `X-Server-Timeout` HTTP header.
+ * @see SystemHeadersBuilder.serverTimeout
+ */
+public const val serverTimeoutHeader: String = "X-Server-Timeout"
+
+/**
+ * Corresponds to the `x-goog-request-params` HTTP header.
+ * @see SystemHeadersBuilder.requestParams
+ */
+public const val requestParamsHeader: String = "x-goog-request-params"
