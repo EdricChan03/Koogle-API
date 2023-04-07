5282464e18d798b612f93532d8dd741fef7fe5a0
Edric <edric.chan.1997@gmail.com>
2023-04-07 22:00:31 +0800

    refactor(plugin/mappings): use Gradle-managed properties

--- buildSrc/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/GenerateMappingsTask.kt
+++ buildSrc/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/GenerateMappingsTask.kt
@@ -1,19 +1,21 @@
 package io.github.edricchan03.koogle_api.plugin.generator
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
-import java.io.File
 
 /** Task that generates Google API mappings. */
 abstract class GenerateMappingsTask : DefaultTask() {
     /** The output directory to output the generated classes to. */
     @get:OutputDirectory
-    abstract val outputDir: File
+    abstract val outputDir: DirectoryProperty
+
     /** The discovery API URL to use. */
     @get:Input
-    abstract val discoveryUrl: String
+    abstract val discoveryUrl: Property<String>
 
     @TaskAction
     fun generateMappings() {
