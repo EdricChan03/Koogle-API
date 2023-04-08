9103d98adbc1841d83262dbdfe2243f6fbbd4b71
Edric <edric.chan.1997@gmail.com>
2023-04-09 05:02:44 +0800

    refactor(buildSrc): remove plugin declaration

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,18 +1,6 @@
 plugins {
-    `java-gradle-plugin`
     `kotlin-dsl`
     kotlin("plugin.serialization") version embeddedKotlinVersion
-}
-
-gradlePlugin {
-    plugins {
-        create("koogleApiGeneratorPlugin") {
-            id = "io.github.edricchan03.koogle-api.generator"
-            displayName = "Koogle API Generator Plugin"
-            description = "Plugin to auto-generate Kotlin sources from the Google API"
-            implementationClass = "io.github.edricchan03.koogle_api.plugin.generator.KoogleApiGeneratorPlugin"
-        }
-    }
 }
 
 dependencies {
