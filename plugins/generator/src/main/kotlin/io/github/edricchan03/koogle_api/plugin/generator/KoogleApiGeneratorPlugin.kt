8007258b95c211975013baf9bf9560139a3c52c4
Edric <edric.chan.1997@gmail.com>
2023-04-09 01:53:02 +0800

    refactor(plugin): extract extension to separate file

--- plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/KoogleApiGeneratorPlugin.kt
+++ plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/KoogleApiGeneratorPlugin.kt
@@ -1,49 +1,13 @@
 package io.github.edricchan03.koogle_api.plugin.generator
 
-import io.github.edricchan03.koogle_api.plugin.generator.data.DirectoryItem
 import io.github.edricchan03.koogle_api.plugin.generator.tasks.DownloadDiscoveriesTask
 import io.github.edricchan03.koogle_api.plugin.generator.tasks.DownloadRootDiscoveryTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.provider.Property
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.registering
-import java.net.URL
-
-abstract class KoogleApiGeneratorPluginExtension {
-    /** The discovery URL to use. */
-    abstract val discoveryUrl: Property<URL>
-
-    /** The discovery URL to use. */
-    fun discoveryUrl(url: String) {
-        discoveryUrl.set(URL(url))
-    }
-
-    /**
-     * The file to output to.
-     * If this value is set to `null`, it will be output to the project's build directory
-     * with the file name "koogle-api-discovery.json".
-     */
-    abstract val rootDiscoveryDocOutputFile: RegularFileProperty
-
-    /**
-     * The directory to save all discovery documents to.
-     * If this value is set to `null`, it will by default be saved to the project's build directory
-     * in a sub-directory "koogle-api-disc-docs".
-     */
-    abstract val discoveryDocsOutputDir: DirectoryProperty
-
-    /** Mapping function used to generate the output file name based from the specified [DirectoryItem]. */
-    abstract val outputFileNameMapper: Property<SchemaFileNameMapper?>
-
-    /** Mapping function used to generate the output file name based from the specified [DirectoryItem]. */
-    fun outputFileNameMapper(mapper: SchemaFileNameMapper) = outputFileNameMapper.set(mapper)
-
-}
 
 /** Plugin that auto-generates the Google HTTP API mappings. */
 class KoogleApiGeneratorPlugin : Plugin<Project> {
