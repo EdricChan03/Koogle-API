8c4b45ada2be0d36a342867eadd5363ac37e49d9
Edric <edric.chan.1997@gmail.com>
2023-04-09 01:53:31 +0800

    docs(plugin): document `Defaults` properties

--- plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/Defaults.kt
+++ plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/Defaults.kt
@@ -1,10 +1,18 @@
 package io.github.edricchan03.koogle_api.plugin.generator
 
+import io.github.edricchan03.koogle_api.plugin.generator.tasks.SchemaFileNameMapper
 import org.gradle.api.Task
 
 object Defaults {
+    /** @see KoogleApiGeneratorPluginExtension.discoveryDocsOutputDir */
     val Task.defaultOutputDiscoveryDocsDir get() = project.layout.buildDirectory.dir("koogle-api-disc-docs")
+
+    /** @see KoogleApiGeneratorPluginExtension.discoveryUrl */
     const val defaultRootDiscoveryUrl = "https://www.googleapis.com/discovery/v1/apis"
+
+    /** @see KoogleApiGeneratorPluginExtension.rootDiscoveryDocOutputFile */
     val Task.defaultRootDiscoveryDoc get() = defaultOutputDiscoveryDocsDir.map { it.file("index.json") }
+
+    /** @see KoogleApiGeneratorPluginExtension.outputFileNameMapper */
     val Task.defaultOutputFileNameMapper get() = SchemaFileNameMapper { "${it.id.replace(":", "-")}.json" }
 }
