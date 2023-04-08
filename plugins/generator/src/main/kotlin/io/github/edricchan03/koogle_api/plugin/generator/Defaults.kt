23bc9f9684d78cc0f7c70c3e90ac7c5ced8f2e18
Edric <edric.chan.1997@gmail.com>
2023-04-09 01:42:04 +0800

    chore(plugin/Defaults): mark URL as `const`

--- plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/Defaults.kt
+++ plugins/generator/src/main/kotlin/io/github/edricchan03/koogle_api/plugin/generator/Defaults.kt
@@ -4,7 +4,7 @@ import org.gradle.api.Task
 
 object Defaults {
     val Task.defaultOutputDiscoveryDocsDir get() = project.layout.buildDirectory.dir("koogle-api-disc-docs")
-    val defaultRootDiscoveryUrl = "https://www.googleapis.com/discovery/v1/apis"
+    const val defaultRootDiscoveryUrl = "https://www.googleapis.com/discovery/v1/apis"
     val Task.defaultRootDiscoveryDoc get() = defaultOutputDiscoveryDocsDir.map { it.file("index.json") }
     val Task.defaultOutputFileNameMapper get() = SchemaFileNameMapper { "${it.id.replace(":", "-")}.json" }
 }
