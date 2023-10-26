c919a3047dc97b82814b486ef7ac6523c78d8d1a
Edric <edric.chan.1997@gmail.com>
2023-10-26 23:11:30 +0800

    fix: migrate back to Dokkatoo
    Forgot to commit this, whoops
    
    * Use type-safe project accessors feature preview

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,16 @@
 plugins {
-    alias(libs.plugins.kotlin.mpp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.dokka)
+    dev.adamko.`dokkatoo-html`
+}
+
+dependencies {
+    dokkatoo("io.github.edricchan03.koogle-api:generator")
+    dokkatoo(projects.koogleApiCommon)
+
+    // TODO: Remove when https://github.com/adamko-dev/dokkatoo/issues/14 is fixed
+    dokkatooPluginHtml(
+        dokkatoo.versions.jetbrainsDokka.map { dokkaVersion ->
+            "org.jetbrains.dokka:all-modules-page-plugin:$dokkaVersion"
+        }
+    )
 }
