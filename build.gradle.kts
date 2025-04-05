93b02c2516df6206467cdb2dc455fc4cfc087594
Edric <edric.chan.1997@gmail.com>
2025-04-05 15:39:07 +0800

    build: update remaining Dokkatoo usages
    aaaaaaaaaaaaaaaaaa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,9 @@
 plugins {
     alias(libs.plugins.kotlin.serialization) apply false
-    dev.adamko.`dokkatoo-html`
+    org.jetbrains.dokka
 }
 
 dependencies {
-    dokkatoo("io.github.edricchan03.koogle-api:generator")
-    dokkatoo(projects.koogleApiCommon)
-
-    // TODO: Remove when https://github.com/adamko-dev/dokkatoo/issues/14 is fixed
-    dokkatooPluginHtml(
-        dokkatoo.versions.jetbrainsDokka.map { dokkaVersion ->
-            "org.jetbrains.dokka:all-modules-page-plugin:$dokkaVersion"
-        }
-    )
+    dokka("io.github.edricchan03.koogle-api:generator")
+    dokka(projects.koogleApiCommon)
 }
